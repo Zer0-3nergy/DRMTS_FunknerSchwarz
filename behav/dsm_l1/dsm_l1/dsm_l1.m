8f5017c2ebe15be7cfc62b4a07f96a71bd48867d
Zer0-3nergy <166703347+Zer0-3nergy@users.noreply.github.com>
2025-11-22 14:42:24 +0100

    updated with files in Aulis
    Added files from Aulis to repo

---
+++ behav/dsm_l1/dsm_l1/dsm_l1.m
@@ -0,0 +1,51 @@
+%% Init
+clc
+clearvars
+clear all
+
+%% Spec.
+N  = 64;                % simulation length (output samples)
+cycles = 2;             % number of sinusoids
+A  = 0.8;               % signal amplitude
+offset = 0;
+fs = 1;                 % sampling frequency 
+Ts  = 1/fs;             % time step
+fx = cycles / (N*Ts);   % signal frequency
+
+t = 0:Ts:(N-1)/fs;
+u = A * sin(2 * pi * fx * t + 2*pi*rand(1));
+
+%% Simulink Simulation
+mdl = 'dsm_l1_sim';
+open_system(mdl);
+
+sigIn.time = t';
+sigIn.signals(1).values = u';
+sigIn.signals(1).dimensions = 1;
+
+simOut = sim(mdl, 'Solver', 'FixedStepDiscrete', ...
+    'StopTime', num2str(max(t)), ...
+    'SaveState','on','StateSaveName','xout',...
+    'SaveOutput','on','OutputSaveName','yout',...
+    'SaveFormat', 'Dataset');
+
+
+% Quantizer gains
+ysim = simOut.yout.get('y').Values.Data';
+vsim = simOut.yout.get('v').Values.Data';
+ksim = (vsim*ysim')/(ysim*ysim');
+
+%% Post-processing
+fig1 = figure()
+plot(t, u, 'rx-', t, ysim, 'g');
+axis([ min(t) max(t) 1.1*min(ysim) 1.1*max(ysim) ]);
+hold on;
+stairs(t, vsim, 'b');
+xlabel('Time t/T');
+ylabel('Amplitude');
+legend('u', 'ysim', 'vsim');
+title('1st Order \Sigma\Delta');
+hold off;
+
+%% Export figure
+%print(fig1, 'lec5s22', '-depsc');
